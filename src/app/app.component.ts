f81ef368218bb4868c8bf7c84a447de0a3b9abe6
Théo Barrier <theo.barrier@hotmail.fr>
2020-04-06 19:54:38 +0200

    Component, @Input, ngFor, ngIf, string interpolation, property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon-projet-angular';
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+  lastUpdate = new Date();
+  appareils = [
+    {
+      name : 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  isAuth = false;
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 1000
+    );
+  }
+
+  onAllumer(){
+    console.log('On allume tout');
+  }
 }
