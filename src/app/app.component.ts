92faf375d3b4a38ae14693c2462506106c41047c
Théo Barrier <theo.barrier@hotmail.fr>
2020-04-07 01:42:39 +0200

    TP : posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,16 @@ export class AppComponent {
   appareilOne = 'Machine à laver';
   appareilTwo = 'Frigo';
   appareilThree = 'Ordinateur';
-  lastUpdate = new Date();
+  date = new Date();
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+          resolve(date);
+        }, 2000
+    );
+  });
+
   appareils = [
     {
       name : 'Machine à laver',
@@ -23,6 +32,27 @@ export class AppComponent {
     {
       name: 'Ordinateur',
       status: 'éteint'
+    }
+  ];
+
+  arrayPosts = [
+    {
+      title: 'Premier post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié.',
+      loveIts: 0,
+      created_at: this.date,
+    },
+    {
+      title: 'Deuxième post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié.',
+      loveIts: 0,
+      created_at: this.date
+    },
+    {
+      title: 'Troisième  post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié.',
+      loveIts: 0,
+      created_at: this.date
     }
   ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule} from '@angular/forms';
+import { PostListComponentComponent } from './post-list-component/post-list-component.component';
+import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MonPremierComponent,
-    AppareilComponent
+    AppareilComponent,
+    PostListComponentComponent,
+    PostListItemComponentComponent
   ],
   imports: [
     BrowserModule,
